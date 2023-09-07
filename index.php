2c6208bde7199c453ccb88e0ba5615183eee65ee
Bademba SANGARE, laptop <sangarebademba.mail@gmail.com>
2023-09-07 20:37:24 +0300

    localhost

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=bfr;charset=utf8;', 'root', 'root');
+$bdd = new PDO('mysql:host=109.234.165.205;dbname=jaje2231_bfr;charset=utf8;', 'jaje2231_bademba', 'FD5isAodQO80NVje2cR&');
 $requete2 = $bdd->query("SELECT * FROM `config` WHERE pop='hight' ORDER BY prix LIMIT 4");
 ?>
 
