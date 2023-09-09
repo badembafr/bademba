6501b117329ac2688c4cb4590148b83251f95af6
Bademba SANGARE, laptop <sangarebademba.mail@gmail.com>
2023-09-09 18:44:00 +0300

    edit local

--- includes/host.php
+++ includes/host.php
@@ -1,17 +1,18 @@
-<!--<?php
+<?php
 
 $host = '109.234.165.205'; 
 $dbname = 'jaje2231_bfr';  
 $id = 'jaje2231_bademba';  
 $passwd = 'FD5isAodQO80NVje2cR&';
 
-?>!-->
+?>
 
-<?php
+
+<!--<?php
 
 $host = 'localhost'; 
 $dbname = 'bfr';  
 $id = 'root';  
 $passwd = 'root';
 
-?>
+?>-->
